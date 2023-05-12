1972ee06f425b6a786d5f2c707254f4381f020bd
chesteric31 <eric.binard.182@gmail.com>
2023-05-12 21:24:39 +0200

    update schema with ia, add getmapping in foto resource

--- src/main/kotlin/be/chesteric31/athena/draco/web/rest/FotoResource.kt
+++ src/main/kotlin/be/chesteric31/athena/draco/web/rest/FotoResource.kt
@@ -4,20 +4,26 @@ import be.chesteric31.athena.draco.domain.Foto
 import be.chesteric31.athena.draco.domain.FotoRepository
 import org.bson.types.Binary
 import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
+
 @RestController
+@RequestMapping("fotos")
 class FotoResource(private val repository: FotoRepository, private val rabbitTemplate: RabbitTemplate) {
 
     @PostMapping("/")
-    fun receiveFotos(@RequestParam(name = "type") type: String, @RequestParam(name = "file") file: MultipartFile) {
-        println("coucou")
+    fun receiveFoto(@RequestParam(name = "type") type: String, @RequestParam(name = "file") file: MultipartFile) {
         var entity = Foto(null, type, Binary(file.bytes))
         entity = repository.save(entity)
-        println(entity.id)
         rabbitTemplate.convertAndSend("topic.fotos", "route", entity.id!!)
     }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    fun getFoto(@PathVariable id: String): ByteArray? {
+        return repository.findByIdOrNull(id)?.image?.data
+    }
+
 }
