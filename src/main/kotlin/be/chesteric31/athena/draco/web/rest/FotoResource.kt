70429e5d615b925cf6e6813238c0f3806aacab29
chesteric31 <eric.binard.182@gmail.com>
2023-05-23 21:21:27 +0200

    update detector.python with detect uri + consuming rabbit + get image and detection update FotoResource.kt with cors and image producing png update Form.vue

--- src/main/kotlin/be/chesteric31/athena/draco/web/rest/FotoResource.kt
+++ src/main/kotlin/be/chesteric31/athena/draco/web/rest/FotoResource.kt
@@ -5,10 +5,12 @@ import be.chesteric31.athena.draco.domain.FotoRepository
 import org.bson.types.Binary
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 
+@CrossOrigin(origins = ["*"], maxAge = 3600)
 @RestController
 @RequestMapping("fotos")
 class FotoResource(private val repository: FotoRepository, private val rabbitTemplate: RabbitTemplate) {
@@ -17,10 +19,11 @@ class FotoResource(private val repository: FotoRepository, private val rabbitTem
     fun receiveFoto(@RequestParam(name = "type") type: String, @RequestParam(name = "file") file: MultipartFile) {
         var entity = Foto(null, type, Binary(file.bytes))
         entity = repository.save(entity)
+        println(type)
         rabbitTemplate.convertAndSend("topic.fotos", "route", entity.id!!)
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}", produces = [MediaType.IMAGE_PNG_VALUE])
     @ResponseBody
     fun getFoto(@PathVariable id: String): ByteArray? {
         return repository.findByIdOrNull(id)?.image?.data
